97b4125603a7bdad7639c34ba8e52d305db35263
Denis <denis@troisdizaines.com>
2017-02-11 13:35:49 +0100

    Add test for root pages

--- tests/test-path.py
+++ tests/test-path.py
@@ -18,33 +18,40 @@ class TestGitWikiPathUrls(unittest.TestCase):
         self.temp_dir.cleanup()
 
     def test_folder_with_index(self):
-        url = "/toto/tutu/"
-        self.ensure_file_presence(url + "index.md")
+        url = '/toto/tutu/'
+        self.ensure_file_presence(url + 'index.md')
         path_info = self.path_manager.get_path_info_from_url(url)
         print("Path = " + path_info.path_on_disk)
-        self.assertEqual(os.path.join(self.temp_dir.name, url[1:], "index.md"), path_info.path_on_disk)
+        self.assertEqual(os.path.join(self.temp_dir.name, url[1:], 'index.md'), path_info.path_on_disk)
 
     def test_folder_without_index(self):
-        url = "/toto/tutu-no-index/"
-        self.ensure_file_presence(url + "no-index")
+        url = '/toto/tutu-no-index/'
+        self.ensure_file_presence(url + 'no-index')
         path_info = self.path_manager.get_path_info_from_url(url)
         self.assertEqual(PathNature.folder_without_index, path_info.pathNature)
 
+    def test_root_page(self):
+        url = ''
+        self.ensure_file_presence('index.md')
+        path_info = self.path_manager.get_path_info_from_url(url)
+        self.assertEqual(PathNature.folder_with_index, path_info.pathNature)
+        self.assertEqual(os.path.join(self.temp_dir.name, 'index.md'), path_info.path_on_disk)
+
     def test_page(self):
         url = "/toto/tutu"
-        self.ensure_file_presence(url + ".md")
+        self.ensure_file_presence(url + '.md')
         path_info = self.path_manager.get_path_info_from_url(url)
         self.assertEqual(os.path.join(self.temp_dir.name, url[1:] + '.md'), path_info.path_on_disk)
 
     def test_page_not_found(self):
-        url = "/toto/tutu-not-found"
+        url = '/toto/tutu-not-found'
         path_info = self.path_manager.get_path_info_from_url(url)
         self.assertEqual(PathNature.not_found, path_info.pathNature)
 
     def test_page_with_space(self):
-        url = "/toto/tu%20tu"
-        decoded_url = "/toto/tu tu"
-        self.ensure_file_presence(decoded_url + ".md")
+        url = '/toto/tu%20tu'
+        decoded_url = '/toto/tu tu'
+        self.ensure_file_presence(decoded_url + '.md')
         path_info = self.path_manager.get_path_info_from_url(url)
         self.assertEqual(os.path.join(self.temp_dir.name, decoded_url[1:] + '.md'), path_info.path_on_disk)
 
@@ -66,8 +73,8 @@ class TestGitWikiPathUrls(unittest.TestCase):
             relative_file_path = file_path[1:]
 
         full_path = os.path.join(self.temp_dir.name, relative_file_path)
-        print("Temp dir : #" + self.temp_dir.name + "#")
-        print("About to create : #" + full_path + "#")
+        print('Temp dir : #' + self.temp_dir.name + '#')
+        print('About to create : #' + full_path + '#')
         basedir = os.path.dirname(full_path)
         if not os.path.exists(basedir):
             os.makedirs(basedir)
