581feec607bd7affb7ab83af3b1cf9abcd1dda5c
Denis R. <denis@troisdizaines.com>
2025-09-29 09:17:30 +0200

    WIP

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -21,6 +21,10 @@ var config = {
                 test: /\.css$/i,
                 use: [miniCssExtractPlugin.loader, 'css-loader'],
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 };
