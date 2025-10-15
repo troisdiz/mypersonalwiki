6d7158be30315481d41530804e18c8eb4ce03f0f
Denis R. <denis@troisdizaines.com>
2025-10-15 20:49:49 +0200

    Unit tests for mermaid parsing

--- tests/test_mermaid_ext.py
+++ tests/test_mermaid_ext.py
@@ -1,36 +1,38 @@
+import os
 import unittest
+
+from markdown.extensions.codehilite import CodeHiliteExtension
 
 from gitwiki.extensions.gitwikimermaid import GitWikiMermaidExtension
 import markdown
 
-text = """
-# Titre 1
-
-```mermaid
-graph TB
-D --> E
-E --> F
-```
-
-"""
-
-EXPECTED_MERMAID_HTML = """<h1>Titre 1</h1>
-<div class="mermaid">
-graph TB
-D --> E
-E --> F
-</div>"""
-
 
 class TestMermaidExtension(unittest.TestCase):
 
-    def test_parse(self):
-        html_content = markdown.markdown(text, extensions=[
+    def run_real_test(self, md_file_name, expected_html_file_name):
+        md_file_path = os.path.join(os.path.dirname(__file__), "testdata", md_file_name)
+        with open(md_file_path, 'r') as md_file:
+            md_file_content = md_file.read()
+
+        expected_html_file_path = os.path.join(os.path.dirname(__file__), "testdata", expected_html_file_name)
+        with open(expected_html_file_path, 'r') as expected_html_file:
+            expected_html_file_content = expected_html_file.read()
+
+        html_content = markdown.markdown(md_file_content, extensions=[
             GitWikiMermaidExtension(),
         ])
 
         print(f"\n---\n{html_content}\n---\n")
-        self.assertEqual(EXPECTED_MERMAID_HTML, html_content)  # add assertion here
+        self.assertEqual(expected_html_file_content, html_content)
+
+    def test_parse(self):
+        self.run_real_test("single-mermaid-block.md", "expected-single-mermaid-block.html")
+
+    def test_parse_two_blocks_mermaid_last(self):
+        self.run_real_test("two-code-blocks-mermaid-last.md", "expected-two-code-blocks-mermaid-last.html")
+
+    def test_parse_two_blocks_mermaid_first(self):
+        self.run_real_test("two-code-blocks-mermaid-first.md", "expected-two-code-blocks-mermaid-first.html")
 
 
 if __name__ == '__main__':
---
+++ tests/testdata/two-code-blocks-mermaid-first.md
@@ -0,0 +1,12 @@
+# Titre 1
+
+```mermaid
+graph TB
+D --> E
+E --> F
+```
+
+```python
+def foo():
+    return "bar"
+```
